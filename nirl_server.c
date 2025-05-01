5d758b20a8b0e699ac356733ae0c89b317f6b84c
Stone071 <zachary.stone15@gmail.com>
2025-04-30 22:59:59 -0400

    Finally figured out HTTP responses so the main page can be served.

--- nirl_server.c
+++ nirl_server.c
@@ -6,14 +6,49 @@
 #include <sys/sendfile.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define NUM_BYTES 1024
+#define NUM_BYTES         1024
+#define TCP_CORK_ENA         1
+#define TCP_CORK_DIS         0
+#define PROTOCOL_TCP         6
 
-char NOT_FOUND_404[] = "HTTP/1.0 404 NOT FOUND";
+char OK_200[]        = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n";
+char CONTENT_HTML[]  = "Content-Type: text/html\r\n";
+char NOT_FOUND_404[] = "HTTP/1.0 404 NOT FOUND\r\n";
+char FORBIDDEN_403[] = "HTTP/1.0 403 FORBIDDEN\r\n";
+
+// Append will memcpy n bytes from src into dest and then return the next
+// open address in dest.
+char* append(char* dest, char* src, size_t n)
+{
+    memcpy(dest, src, n);
+    return (dest+n);
+}
+
+bool isFavicon(char* filename)
+{
+    char refName[] = "favicon.ico";
+    bool isMatch = true;
+
+    // "favicon.ico" is a has 12 chars because of the null terminator.
+    // We do not want to compare against the null terminator, so cut
+    // the comparison one index short.
+    for (int i=0; i<sizeof(refName)-1; i++)
+    {
+        if (filename[i] != refName[i])
+        {
+            isMatch = false;
+            break;
+        }
+    }
+    return isMatch;
+}
 
 int main()
 {
@@ -34,17 +69,22 @@ int main()
     // Bind the port to the socket
     if (bind(s, &port_desc, sizeof(port_desc)) < 0)
     {
-        perror("BIND ERR\n");
+        perror("BIND ERR");
     }
     else
     {
         // Let's just sequentially serve all connections.
         while(1)
         {  
+            bool GetAndSend = true;
             // Listen for a connection
             if (listen(s, 10) < 0) // 10 is number of waiting connections before declining
             {
                 perror("LISTEN ERR");
+            }
+            else
+            {
+                printf("Listening for connection...\n");
             }
 
             int client_fd = accept(s, 0, 0);
@@ -55,34 +95,73 @@ int main()
                 perror("RECV ERR");
             }
             // Print the request because I am curious.
-            printf("\nHTTP REQUEST:\n%s", buffer);
+            //printf("\nHTTP REQUEST:\n%s", buffer);
 
             // GET /file.html ....
             char* filename = buffer + 5;
-            // Set the space after the filename to 0 for null terminated
-            *strchr(filename, ' ') = 0;
-            int opened_fd = open(filename, O_RDONLY);
-            if (opened_fd == -1)
+            char* firstSpace = strchr(filename, ' ');
+            // Handle specific file cases
+            if (firstSpace == filename)
             {
-                // send back 404 not found
-                send(client_fd, NOT_FOUND_404, sizeof(NOT_FOUND_404), 0);
-                perror("OPEN FILE ERR");
+                // If pointer to the first space in the string is the same as filename,
+                // then user has not requested a specific page, just the base address.
+                filename = "public/index.html";
+            }
+            else if(isFavicon(filename))
+            {
+                // It is requesting favicon. If you say 404 not found the page won't load.
+                GetAndSend = false;
+                // send back 200 ok?
+                send(client_fd, OK_200, sizeof(OK_200), 0);
             }
             else
             {
-                // Find the size of the file
-                struct stat* fInfo = malloc(sizeof(struct stat));
-                int fSize = fstat(opened_fd, fInfo);
-                
-                // Send the content of the file to the client socket.
-                if (sendfile(client_fd, opened_fd, 0, fInfo->st_size) < 0)
-                {
-                    perror("SENDFILE ERR");
-                }
+                // Set the space after the filename to 0 for null terminated
+                *firstSpace = 0;
             }
 
-            // Close everything
-            close(opened_fd);
+            // Should perform a check here to see if the file they have requested
+            // is allowed, or should not be accessible to them.
+            if (GetAndSend)
+            {
+                int opened_fd = open(filename, O_RDONLY);
+                if (opened_fd == -1)
+                {
+                    // send back 404 not found
+                    send(client_fd, NOT_FOUND_404, sizeof(NOT_FOUND_404), 0);
+                    perror("OPEN FILE ERR");
+                }
+                else
+                {
+                    // Find the size of the file
+                    struct stat* fInfo = malloc(sizeof(struct stat));
+                    int fSize = fstat(opened_fd, fInfo);
+                    // Allocate a response buffer
+                    size_t bufSize = sizeof(OK_200) + fInfo->st_size;
+                    char* rspBuf = malloc(bufSize);
+                    // Put the HTTP response in the buffer
+                    // Okay so one critical component seems to be that the HTTP response
+                    // must have two \n\n before the content comes, and standard is \r\n\r\n.
+                    // Whether or not the null terminator is included in the response strings
+                    // does not seem to affect anything.
+                    char* pNext = append(rspBuf, OK_200, sizeof(OK_200));
+                    
+                    // Put the file content in the buffer
+                    read(opened_fd, pNext, fInfo->st_size);
+
+                    // Send it off!
+                    send(client_fd, rspBuf, bufSize, 0);
+
+                    printf("%s", rspBuf);
+                    
+                    free(rspBuf);
+                    free(fInfo);
+                }
+                // Close file
+                close(opened_fd);
+            }
+
+            // Close client connection
             close(client_fd);   
         }
     }
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 * Consider multithreading for serving several simultaneous connections
 * Make a whitelist for pages we are allowed to serve and only serve those pages
 * Figure out how to serve HTTPS connections
+* Figure out how to prepend the HTTP return code to the file you are serving to send it all as one stream.
+* Parse the actual request to determine if GET or POST
+* Set up index.html to show up as the homepage.
